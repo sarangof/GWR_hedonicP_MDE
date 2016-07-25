8271de6c4826edfeeecb6db52bce62cb700becda
Sara Arango <sarangofc@gmail.com>
2016-07-24 21:11:17 -0400

    First commit.

---
+++ code/main.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+
+data = pd.read_csv('/home/saf537/Documents/GWR_hedonicP_MDE/Data/RE_MDE.csv')
+
+
+
+""" Data standardizing etc."""
+
+
+""" Exploratory plots """
+
+
+""" Feature selection """
